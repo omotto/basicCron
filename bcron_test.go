0c0ef740503e69c6a72b6312483ff9f619b18ec0
Oscar Motto <oscar.motto@forcemanager.net>
2020-11-02 16:48:13 +0100

    Added new tests

--- bcron_test.go
+++ bcron_test.go
@@ -82,7 +82,7 @@ func TestCronBasic(t *testing.T) {
 	cron.Stop()
 }
 
-func TestCronSchedule(t *testing.T) {
+func TestCronNormalSchedule(t *testing.T) {
 	testN := 0
 	testS := ""
 
@@ -119,5 +119,169 @@ func TestCronSchedule(t *testing.T) {
 	if testS != "param" {
 		t.Error("func 2 not executed as scheduled")
 	}
+}
+
+func TestCronAddAfterStopSchedule(t *testing.T) {
+	testN := 0
+
+	cron := New(time.Second*2)
+	cron.Start()
 	cron.Stop()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	if _, err := cron.AddFunc(time.Now().Add(time.Second*2), time.Second*10, func() { testN++; wg.Done() }); err != nil {
+		t.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-time.After(time.Second * 5):
+		if testN > 0 {
+			t.Fatal("expected stopped cron does not run any job")
+		}
+	case <-done:
+		t.Fatal("expected stopped cron does not run any job")
+	}
+}
+
+func TestCronAddBeforeStartSchedule(t *testing.T) {
+	testN := 0
+	testS := ""
+
+	cron := New(time.Second*2)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	if _, err := cron.AddFunc(time.Now().Add(time.Second*4), time.Second*10, func() { testN++; wg.Done() }); err != nil {
+		t.Fatal(err)
+	}
+
+	cron.Start()
+
+	time.Sleep(2 * time.Second)
+
+	if _, err := cron.AddFunc(time.Now().Add(time.Second*3), time.Second*10, func(s string) { testS = s; wg.Done() }, "param"); err != nil {
+		t.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(5 * time.Second):
+	}
+
+	if testN != 1 {
+		t.Error("func 1 not executed as scheduled")
+	}
+
+	if testS != "param" {
+		t.Error("func 2 not executed as scheduled")
+	}
+}
+
+
+func TestCronRemoveBeforeStartSchedule(t *testing.T) {
+	testN := 0
+	testS := ""
+
+	cron := New(time.Second*2)
+
+	var (
+		err error
+		id	string
+		wg sync.WaitGroup
+	)
+
+	wg.Add(2)
+
+	if id, err = cron.AddFunc(time.Now().Add(time.Second), time.Second, func() { testN++; wg.Done() }); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = cron.AddFunc(time.Now().Add(time.Second*3), time.Second*10, func(s string) { testS = s; wg.Done() }, "param"); err != nil {
+		t.Fatal(err)
+	}
+
+	cron.DelFunc(id)
+
+	cron.Start()
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		 t.Fatal("expected stopped cron does not run first job")
+	case <-time.After(5 * time.Second):
+	}
+
+	if testN != 0 {
+		t.Error("func 1 must be removed")
+	}
+
+	if testS != "param" {
+		t.Error("func 2 not executed as scheduled")
+	}
+}
+
+func TestCronRemoveAfterStartSchedule(t *testing.T) {
+	testN := 0
+	testS := ""
+
+	cron := New(time.Second*2)
+
+	var (
+		err error
+		id	string
+		wg sync.WaitGroup
+	)
+
+	wg.Add(1)
+
+	if id, err = cron.AddFunc(time.Now().Add(time.Second), time.Second, func() { testN++ }); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = cron.AddFunc(time.Now().Add(time.Second*3), time.Second*10, func(s string) { testS = s; wg.Done() }, "param"); err != nil {
+		t.Fatal(err)
+	}
+
+	cron.Start()
+
+	time.Sleep(time.Second*5)
+	cron.DelFunc(id)
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(5 * time.Second):
+	}
+
+	if testN > 2 {
+		t.Error("func 1 must be removed before 3th tick")
+	}
+
+	if testS != "param" {
+		t.Error("func 2 not executed as scheduled")
+	}
 }
