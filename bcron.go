ad564abd24b5df6e05ad097a7916b290dd7ff4ea
Oscar Motto <oscar.motto@forcemanager.net>
2020-11-02 15:24:05 +0100

    Updated documentation

--- bcron.go
+++ bcron.go
@@ -1,7 +1,6 @@
-package basicCron
+package basiccron
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"reflect"
@@ -9,7 +8,7 @@ import (
 	"time"
 )
 
-type Job struct {
+type job struct {
 	uuid			string
 	triggerDate		time.Time
 	period			time.Duration
@@ -17,66 +16,77 @@ type Job struct {
 	fparams			[]interface{}
 }
 
+// Cron Struct
 type Cron struct {
-	lastIndex		int64
-	running			bool
-	stop			chan bool
-	period			time.Duration
-	jobs 			[]Job
-	addJob			chan Job
-	delJob			chan string
+	lastIndex		int64				// Last inserted job index
+	running			bool				// Scheduler status running (true) or stopped
+	stop			chan bool			// Stop channel
+	period			time.Duration		// Period of time between scheduler analysis
+	jobs 			[]job				// Slice of jobs to be executed
+	addJob			chan job			// Add Job Channel
+	delJob			chan string			// Del Job Channel
 }
 
-
-/**
- * Creates a new Cron instance
- *
- * @param 	time.Duration	: period of time between time check
- * @return 	*Cron			: new Cron object
- */
+// New Creates a new Cron instance
+// params:
+// 		time.Duration	: period of time between time check
+// returns:
+//		*Cron			: new Cron object
 func New(period time.Duration) *Cron {
 	return &Cron{
 		running: false,
 		period : period,
 		stop: make(chan bool),
-		addJob: make(chan Job),
+		addJob: make(chan job),
 		delJob: make(chan string),
 		lastIndex: 0,
 	}
 }
 
-// AddFunc adds a func to the Cron to be executed
 
+
+// AddFunc adds a func to Cron to be executed
+// params:
+//		time.Time		: Job's execution start time
+//		time.Duration 	: Period between job execution
+//		interface{}		: function to execute
+//		...interface{}  : input argument list of the attached function
+// returns:
+//		string			: UUID to identify this new created Job
+//		err 			: error value (nil if no error)
 func (c *Cron) AddFunc(triggerDate time.Time, period time.Duration, function interface{}, fparams ...interface{}) (uuid string, err error) {
 	// Check input parameters
 	// -- Check first trigger date is in the future
-	if triggerDate.Before(time.Now()) {	return uuid, errors.New("triggerDate must be in the future") }
+	if triggerDate.Before(time.Now()) {	return uuid, fmt.Errorf("triggerDate must be in the future") }
 	// -- Check if param function contains a function
-	if function == nil || reflect.ValueOf(function).Kind() != reflect.Func { return uuid, errors.New("invalid function parameter") }
+	if function == nil || reflect.ValueOf(function).Kind() != reflect.Func { return uuid, fmt.Errorf("invalid function parameter") }
 	// -- Check number of params
-	if len(fparams) != reflect.TypeOf(function).NumIn() { return uuid, errors.New("number of function params and number of provided params doesn't match") }
+	if len(fparams) != reflect.TypeOf(function).NumIn() { return uuid, fmt.Errorf("number of function params and number of provided params doesn't match") }
 	// -- Check input params type belongs to function params type
 	for i := 0; i < reflect.TypeOf(function).NumIn(); i++ {
 		functionParam := reflect.TypeOf(function).In(i)
 		inputParam := reflect.TypeOf(fparams[i])
 		if functionParam != inputParam {
-			if functionParam.Kind() != reflect.Interface { return uuid, errors.New(fmt.Sprintf("param[%d] must be be `%s` not `%s`", i, functionParam, inputParam)) }
-			if !inputParam.Implements(functionParam) { return uuid, errors.New(fmt.Sprintf("param[%d] of type `%s` doesn't implement interface `%s`", i, functionParam, inputParam)) }
+			if functionParam.Kind() != reflect.Interface { return uuid, fmt.Errorf(fmt.Sprintf("param[%d] must be be `%s` not `%s`", i, functionParam, inputParam)) }
+			if !inputParam.Implements(functionParam) { return uuid, fmt.Errorf(fmt.Sprintf("param[%d] of type `%s` doesn't implement interface `%s`", i, functionParam, inputParam)) }
 		}
 	}
 	// Add new job
 	uuid = strconv.FormatInt(time.Now().UnixNano(), 16) + strconv.FormatInt(c.lastIndex, 16)
 	c.lastIndex++
-	job := Job { uuid: uuid, triggerDate: triggerDate, period: period, function: function, fparams: fparams }
+	newJob := job { uuid: uuid, triggerDate: triggerDate, period: period, function: function, fparams: fparams }
 	if c.running == true {
-		c.addJob <- job
+		c.addJob <- newJob
 	} else {
-		c.jobs = append(c.jobs, job)
+		c.jobs = append(c.jobs, newJob)
 	}
 	return uuid, err
 }
 
 
+// DelFunc removes a func from Cron
+// params
+//		string			: UUID to identify the job to be removed
 func (c *Cron) DelFunc(uuid string) {
 	if c.running == true {
 		c.delJob <- uuid
@@ -85,22 +95,7 @@ func (c *Cron) DelFunc(uuid string) {
 	}
 }
 
-func (c *Cron) delFunc(uuid string) {
-	var index int = -1
-	for i := 0; i < len(c.jobs); i++ {
-		if c.jobs[i].uuid == uuid {
-			index = i
-			break
-		}
-	}
-	if index != -1 {
-		c.jobs[index] = c.jobs[len(c.jobs)-1]
-		c.jobs = c.jobs[:len(c.jobs)-1]
-	}
-}
-
-// Stops Cron scheduler
-
+// Stop stops Cron scheduler
 func (c *Cron) Stop() {
 	if c.running == true {
 		c.stop <- true
@@ -108,8 +103,7 @@ func (c *Cron) Stop() {
 	}
 }
 
-// Starts Cron scheduler
-
+// Start starts Cron scheduler
 func (c *Cron) Start() {
 	if c.running == false {
 		c.running = true
@@ -125,9 +119,9 @@ func (c *Cron) Start() {
 							go c.execJob(j)
 						}
 					}
-				case job := <-c.addJob:
+				case newJob := <-c.addJob:
 					ticker.Stop()
-					c.jobs = append(c.jobs, job)
+					c.jobs = append(c.jobs, newJob)
 					ticker = time.NewTicker(c.period)
 				case uuid := <-c.delJob:
 					ticker.Stop()
@@ -143,7 +137,7 @@ func (c *Cron) Start() {
 
 // Private Methods
 
-func (c *Cron) execJob(j Job) {
+func (c *Cron) execJob(j job) {
 	defer func() {
 		if r := recover(); r != nil { log.Println("cron exec func ", j.uuid, " error ", r) }
 	}()
@@ -152,4 +146,18 @@ func (c *Cron) execJob(j Job) {
 		args[i] = reflect.ValueOf(param)
 	}
 	reflect.ValueOf(j.function).Call(args)
+}
+
+func (c *Cron) delFunc(uuid string) {
+	var index = -1
+	for i := 0; i < len(c.jobs); i++ {
+		if c.jobs[i].uuid == uuid {
+			index = i
+			break
+		}
+	}
+	if index != -1 {
+		c.jobs[index] = c.jobs[len(c.jobs)-1]
+		c.jobs = c.jobs[:len(c.jobs)-1]
+	}
 }
